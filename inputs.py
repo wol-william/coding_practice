9675ac8ecc739a9d6a9ca8bdb8b12327d0b0dd49
gkiplangat <gkiplangat>
2023-02-18 12:35:52 +0300

    inputs.py updated

--- inputs.py
+++ inputs.py
@@ -12,3 +12,8 @@ num2= int(input("Enter the second number: "))
 #calculate
 result = num1 + num2
 print(result)
+
+# calculate the difference of the two numbers
+result2 = num1 - num2
+print(result2)
+
