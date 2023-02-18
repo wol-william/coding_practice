0590247b27ad3ae1bc6ffb988c801df1d5bbe23d
William Wol <125727093+wol-william@users.noreply.github.com>
2023-02-18 12:10:33 +0300

    inputs.py
    created a new file

---
+++ inputs.py
@@ -0,0 +1,14 @@
+#name = input("What is your name: ")
+
+#print("Pleasure to meet you "+name)
+#age = input("How old are you? ")
+#print("Wooo! ")
+
+
+# calculate the sum of two numbers
+num1= int(input("Enter the first number: "))
+num2= int(input("Enter the second number: "))
+
+#calculate
+result = num1 + num2
+print(result)
