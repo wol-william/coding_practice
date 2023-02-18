8df98e960749c433349f3a6787d842dd7a118ccb
William Wol <125727093+wol-william@users.noreply.github.com>
2023-02-18 12:19:04 +0300

    operators
    updates

---
+++ operators.py
@@ -0,0 +1,14 @@
+
+#division operator = /
+#multiplication operator = *
+#Addition operator = +
+#Subtraction operator
+
+# create a new file called division.py
+   # write a code in python that divide two numbers and print out the result
+# create a new file called multiplication.py
+   #write a code in python that multiply two numbers and print the result
+#Create a new file called subtraction.py
+   #write a code in python that subtract two numbers and print the results.
+
+#all the new files should be committed to github account
