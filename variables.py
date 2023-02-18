8ce2536a570c9b8a1656584b82660e86dfb99005
William Wol <125727093+wol-william@users.noreply.github.com>
2023-02-18 11:55:05 +0300

    variable.py
    updated the variable.py

---
+++ variables.py
@@ -0,0 +1,11 @@
+#create a name variable
+y = "William Wol"  #variable declaration
+
+#print the content of y
+print(y)
+print(y[0])
+print(y[9])
+print(y[7])
+
+#Change the content of variable y into uppercase
+print(len(y))
