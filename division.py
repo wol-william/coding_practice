b77d415fb8ad8c347170df775e54c6e127879684
gkiplangat <gkiplangat>
2023-02-22 12:36:19 +0300

    Perform divion in division.py

---
+++ division.py
@@ -0,0 +1,21 @@
+#Qst: write a code in python that divide two numbers and print the result
+#Step 1: create two variables to hold in each number from the user
+#Step 2: Change the the numbers into integer for calculation
+#Step 3: Create another variable to hold the result and 
+#perform calculation (divide the two numbers)
+#Store the answer in result variable
+#Step 4: Print the result
+
+#Step 1
+num1 = input("Enter your first number: ")
+num2 = input("Enter your second number: ")
+
+#Step 2
+num1 = int(num1)
+num2 = int(num2)
+
+#Step 3
+result = num1 / num2
+
+#Step 4
+print(result)
